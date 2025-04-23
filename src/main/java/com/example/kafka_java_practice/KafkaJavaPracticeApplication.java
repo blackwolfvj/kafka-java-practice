67b3f664ba83de1b4b5e141aa0d8863835863e09
Thamilarasu VJ <thamilarasu.vj@ibm.com>
2025-04-23 12:11:08 +0530

    Initial Commit

---
+++ src/main/java/com/example/kafka_java_practice/KafkaJavaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_java_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJavaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafka_java_practice/KafkaJavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_java_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
